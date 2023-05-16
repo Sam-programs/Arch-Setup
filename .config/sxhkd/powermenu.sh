7cfd422e70fe298b7fd44d79646847b632b08f87
Sam <130783534+Sam-programs@users.noreply.github.com>
2023-05-16 18:07:17 +0300

    renamed log out to switch users

--- .config/sxhkd/powermenu.sh
+++ .config/sxhkd/powermenu.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-choice=$(printf "Cancel\nShutDown\nRestart\nLog Out\nSleep" | rofi -dmenu -i -p "PowerMenu:")
+choice=$(printf "Cancel\nShutDown\nRestart\nSwitch Users\nSleep" | rofi -dmenu -i -p "PowerMenu:")
 
 case "$choice" in 
    "Cancel")   exit 0;;
    "ShutDown") systemctl shutdown now;;
    "Restart")  systemctl reboot -i;;
    "Sleep")    systemctl suspend;;
-   "switch users")  bspc quit;;
+   "Switch Users")  bspc quit;;
    *)exit 1 ;;
 esac
