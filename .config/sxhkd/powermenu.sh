aa936fa761f399ba3260de10c50755dd507c2be4
Sam <130783534+Sam-programs@users.noreply.github.com>
2023-05-16 18:03:59 +0300

    added sleep mode

--- .config/sxhkd/powermenu.sh
+++ .config/sxhkd/powermenu.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
-choice=$(printf "Cancel\nShutDown\nRestart\nLog Out" | rofi -dmenu -i -p "PowerMenu:")
+choice=$(printf "Cancel\nShutDown\nRestart\nLog Out\nSleep" | rofi -dmenu -i -p "PowerMenu:")
 
 case "$choice" in 
    "Cancel")   exit 0;;
    "ShutDown") systemctl shutdown now;;
    "Restart")  systemctl reboot -i;;
+   "Sleep")    systemctl suspend;;
    "Log Out")  bspc quit;;
    *)exit 1 ;;
 esac
