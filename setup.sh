be360d36a10b4c01194c1b2084ffae729e5c4180
Sam <130783534+Sam-programs@users.noreply.github.com>
2023-05-16 17:26:25 +0300

    removed huge default packages

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo pacman -S lightdm lightdm-slick-greeter
 #you'ill have to use startx to load in bspwm
 function lightdm_launch(){
  ln -s -f ~/.xinitrc ~/.xsession
- sudo chmod +x ~/.xinitrc ~/.xsession
+ sudo chmod +x ~/.xsession #just in case
  sudo systemctl enable --now lightdm 
 }
 
@@ -34,19 +34,21 @@ mkdir ~/.config/
 mkdir ~/.config/copyq
 #
 
-sudo pacman -S zip
+#sudo pacman -S zip
 
-
-sudo pacman -S clang lld libc++ gdb
+#default off
+#sudo pacman -S clang lld libc++ gdb
 #seting lld as the linker
-sudo cp -f /usr/bin/ld /usr/bin/ld-orginal
-sudo ln -s -f /usr/bin/ld.lld /usr/bin/ld
+#sudo cp -f /usr/bin/ld /usr/bin/ld-orginal
+#sudo ln -s -f /usr/bin/ld.lld /usr/bin/ld
 
 #neovim setup
 sudo pacman -S neovim xsel
 sudo git clone https://github.com/Sam-programs/simple-neovim-setup
 sudo chmod +x ./simple-neovim-setup/setup.sh
 ./simple-neovim-setup/setup.sh
+
+mkdir ~/code/
 
 sudo pacman -S pulseaudio
 
@@ -57,13 +59,11 @@ sudo pacman -S xorg-server xorg-xinit
 sudo pacman -S alacritty
 sudo pacman -S ttf-firacode-nerd vim #vim here is for manpages and hotkey manual in xinitrc
 
+mkdir ~/code/
+
 cp -f ./.xinitrc ~
 sudo chmod +x ~/.xinitrc
 
-mkdir ~/code/
-mkdir ~/Downloads/
-
 cp -rf ./.config/ ~
-
 
 lightdm_launch
