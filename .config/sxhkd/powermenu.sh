fe0de7fbc11129b69c04a78386af8e3ddf4a9d78
Sam <130783534+Sam-programs@users.noreply.github.com>
2023-05-16 18:09:56 +0300

    readded log out

--- .config/sxhkd/powermenu.sh
+++ .config/sxhkd/powermenu.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
-choice=$(printf "Cancel\nShutDown\nRestart\nSwitch Users\nSleep" | rofi -dmenu -i -p "PowerMenu:")
+choice=$(printf "Cancel\nShutDown\nRestart\nSwitch Users\nSleep\nLog Out" | rofi -dmenu -i -p "PowerMenu:")
 
 case "$choice" in 
    "Cancel")   exit 0;;
    "ShutDown") systemctl shutdown now;;
    "Restart")  systemctl reboot -i;;
    "Sleep")    systemctl suspend;;
+   "Log Out")  xfce4-screensaver-command -l;;
    "Switch Users")  bspc quit;;
    *)exit 1 ;;
 esac
